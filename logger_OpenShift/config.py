416fe5acf61e3d481292dc8f8b1cbc10bb1214b6
amaich <61798099+amaich@users.noreply.github.com>
2023-03-23 11:48:25 +0300

    Update config.py

--- logger_OpenShift/config.py
+++ logger_OpenShift/config.py
@@ -1,19 +1,19 @@
 #первая квота
-os_cluster_1 = 'prom-terra000083-i39m'
-os_project_1 = 'ci02473893-pprb4-samo-synapse-megacod'
-os_token_1 = 'Bearer sha256~ua5fphyQ9b3wrUadOVmyKwHRlXN4E78KJfFhlZa-Rmc'
+os_cluster_1 = '***'
+os_project_1 = '***'
+os_token_1 = '***'
 
 #вторая квота
-os_cluster_2 = 'prom-terra000082-i39s'
-os_project_2 = 'ci02473893-pprb4-samo-synapse-skol'
-os_token_2 = 'Bearer sha256~H-r-Mo7udJwuRlxiUMgshZ_9Ukcl91ukataMEHCQeoo'
+os_cluster_2 = '***'
+os_project_2 = '***'
+os_token_2 = '***'
 
 #словарь в котором хранятся имена подов и контейнеров, с которых нужно собирать логи в формате:
 #ИМЯ_ПОДА: ИМЯ_КОНТЕЙНЕРА
 os_pod_container = {
-    'istio-egressgateway': 'istio-proxy',
-    'istio-ingressgateway': 'istio-proxy',
-    'samo-synapse-tasks': 'samo-synapse-tasks',
-    'samo-synapse-facade': 'samo-synapse-facade',
+    'istio-egressgateway': '***',
+    'istio-ingressgateway': '***',
+    'samo-synapse-tasks': '***',
+    'samo-synapse-facade': '***',
 
 }
